ec47793cd5a17f66f96285744b9176ae59503edb
Hossein Arshad <65465291+optimanalytic@users.noreply.github.com>
2025-01-03 20:26:49 +0100

    Update test_mypackage.py

--- mymath/test_mypackage.py
+++ mymath/test_mypackage.py
@@ -1,3 +1,5 @@
+# This code is part of the IBM Data Engineering Professional Certificate course on Coursera.
+
 import mymath
 print(mymath.basic.add(3, 4))  # Should output: 7
 print(mymath.stats.mean([3, 4, 5]))  # Should output: 4.0
